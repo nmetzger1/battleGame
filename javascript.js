532fdbffc98e7f0622e13fff08b5b77b708d3ee2
Nick <numbnick2@gmail.com>
2017-02-23 00:01:04 -0500

    gameplay is working, need to add a restart function

--- javascript.js
+++ javascript.js
@@ -1,92 +1,106 @@
 //Character Objects
-var Luke = {
-    name: "Luke Skywalker",
-    id: "luke",
+var boshek = {
+    name: "BoShek",
+    id: "boshek",
     attack: 8,
     hp: 110,
-    img: "images/luke.png",
+    img: "images/boshek.png",
     player: false,
     opp: false,
     defeated: false
 };
-var Han = {
-    name: "Han Solo",
-    id: "han",
+var lobot = {
+    name: "Lobot",
+    id: "lobot",
     attack: 25,
-    hp: 110,
-    img: "images/han.png",
+    hp: 50,
+    img: "images/lobot.png",
     player: false,
     opp: false,
     defeated: false
 };
 
-var Kenobi = {
-    name: "Obi-Wan Kenobi",
-    id: "kenobi",
-    attack: 8,
-    hp: 110,
-    img: "images/kenobi.png",
+var porkins = {
+    name: "Jek Porkins",
+    id: "porkins",
+    attack: 10,
+    hp: 93,
+    img: "images/porkins.png",
     player: false,
     opp: false,
     defeated: false
 };
 
-var Vader = {
-    name: "Darth Vader",
-    id: "vader",
-    attack: 8,
-    hp: 110,
-    img: "images/vader.png",
+var max = {
+    name: "Max Rebo",
+    id: "max",
+    attack: 13,
+    hp: 80,
+    img: "images/max.png",
     player: false,
     opp: false,
     defeated: false
 };
 
-characterArray = [Luke, Han, Kenobi, Vader];
+characterArray = [boshek, max, lobot, porkins];
 
 // End of Character definitions
 
 //Global Variables
-var isPlayerSelected = false;
-var isOppSelected = false;
+var isPlayerSelected;
+var isOppSelected;
 var player = {};
 var currentOpponent = {};
-var attackCounter = 1;
+var attackCounter;
+
+function gameStart() {
+    isPlayerSelected = false;
+    isOppSelected = false;
+    player = {};
+    currentOpponent = {};
+    attackCounter = 1;
+
+    for(var i in characterArray){
+        createCharacterDiv(characterArray[i],"char-select")
+    }
+    playerSelect();
+}
 
 
 function playerSelect() {
-    $(".user-char").click(function (charClick) {
+    $(".img-div-char-select").click(function (charClick) {
 
         if(isPlayerSelected){
             return;
         }
 
         switch (charClick.target.id){
-            case "luke":
-                Luke.player = true;
-                player = Luke;
+            case "img-char-select-boshek":
+                boshek.player = true;
+                player = boshek;
                 break;
-            case "han":
-                Han.player = true;
-                player = Han;
+            case "img-char-select-max":
+                max.player = true;
+                player = max;
                 break;
-            case "kenobi":
-                Kenobi.player = true;
-                player = Kenobi;
+            case "img-char-select-porkins":
+                porkins.player = true;
+                player = porkins;
                 break;
             default:
-                Vader.player = true;
-                player = Vader;
+                lobot.player = true;
+                player = lobot;
         }
 
-        $('<img src="' + player.img + '">').appendTo(".player");
+        $('<img src="' + player.img + '">').appendTo("#player-img");
+        $('.player-stats').html("Atk: " + player.attack + " HP: " + player.hp);
 
-        for(i = 0; i < characterArray.length; i++){
+        for(var i in characterArray)
+        {
             if(characterArray[i].player == false){
-                $('<img src="' + characterArray[i]  .img + '" class="opp" id= opp-' + characterArray[i].id + '>').appendTo(".opponents");
+                createCharacterDiv(characterArray[i],"opp-list");
                 characterArray[i].opp = true;
             }
-            $("#" + characterArray[i].id).hide();
         }
 
         isPlayerSelected = true;
@@ -95,29 +109,29 @@ function playerSelect() {
 }
 
 function opponentSelect() {
-    $(".opp").click(function (oppClick) {
+    $(".img-div-opp-list").click(function (oppClick) {
 
         if (isOppSelected) {
             return;
         }
 
         switch (oppClick.target.id) {
-            case "opp-luke":
-                currentOpponent = Luke;
+            case "img-opp-list-boshek":
+                currentOpponent = boshek;
                 break;
-            case "opp-han":
-                currentOpponent = Han;
+            case "img-opp-list-max":
+                currentOpponent = max;
                 break;
-            case "opp-kenobi":
-                currentOpponent = Kenobi;
+            case "img-opp-list-porkins":
+                currentOpponent = porkins;
                 break;
             default:
-                currentOpponent = Vader;
+                currentOpponent = lobot;
         }
 
-            $('<img src="' + currentOpponent.img + '" id="current-' + currentOpponent.id + '">').appendTo(".current-opponent");
-            isOppSelected = true;
-            $("#opp-" + currentOpponent.id).hide();
+        createCharacterDiv(currentOpponent,"current-opponent");
+        isOppSelected = true;
+        $("#div-opp-list-" + currentOpponent.id).hide();
 
     });
 
@@ -132,6 +146,18 @@ function battle() {
     });
 }
 
+function createCharacterDiv(object, panel){
+
+    var div = $("<div class='img img-div-" + panel + "' id='div-" + panel + "-" + object.id + "'>");
+    var image = $("<img src='" + object.img + "' class='image image-" + panel + "' id='img-" + panel + "-" + object.id + "'>");
+    var stats = $("<p class = 'stats panel-" + panel + "'> Atk: " + object.attack + " HP: " + object.hp + "</p>");
+
+    $("." + panel).append(div);
+    div.append(image, stats);
+
+}
+
+
 function attack() {
 
     if(isOppSelected == false){
@@ -140,16 +166,15 @@ function attack() {
 
     currentOpponent.hp -= (player.attack * attackCounter);
     player.hp -= currentOpponent.attack;
-    console.log("Attack", player.attack * attackCounter);
-    console.log("Opp HP", currentOpponent.hp);
-    console.log("Player HP", player.hp);
     attackCounter++;
+    $('.player-stats').html("Atk: " + (player.attack * attackCounter) + " HP: " + player.hp);
+    $('.panel-current-opponent').html("Atk: " + currentOpponent.attack + " HP: " + currentOpponent.hp);
 }
 
 
 function checkForOppDefeat(){
     if(currentOpponent.hp < 1){
-        $("#current-"+currentOpponent.id).hide();
+        $(".img-div-current-opponent").hide();
         isOppSelected = false;
 
         for(var i in characterArray){
