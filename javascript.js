a0fb1d6f990be98e2e1f23307c6ef72e2079c681
Nick <numbnick2@gmail.com>
2017-02-25 01:18:29 -0500

    added a bunch o' comments

--- javascript.js
+++ javascript.js
@@ -48,8 +48,8 @@ characterArray = [boshek, max, lobot, porkins];
 // End of Character definitions
 
 //Global Variables
-var isPlayerSelected;
-var isOppSelected;
+var isPlayerSelected; //Manages game state
+var isOppSelected;  //Manages game state
 var player = {};
 var currentOpponent = {};
 var attackCounter;
@@ -64,19 +64,24 @@ function gameStart() {
     setPlayerStats();
 
     $(".char-select").html("<h1>Player Select</h1>");
+
+    //Create an image div (image + stats) for all characters
     for(var i in characterArray){
         createCharacterDiv(characterArray[i],"char-select")
     }
 
+    //Empty all game areas except for character select
     $(".middle").empty();
     $(".player").empty();
     $(".opp-list").empty();
     $(".current-opponent").empty();
+    $(".game-info");
 
 
     playerSelect();
 }
 
+//Resets all characters back to their starting values
 function setPlayerStats(){
 
     boshek.hp = 107;
@@ -99,15 +104,19 @@ function setPlayerStats(){
 
 }
 
+//Handles player selection portion of the game
 function playerSelect() {
     $(".img-div-char-select").click(function (charClick) {
 
-        var chosenCharacter = charClick.currentTarget.id;
 
+        //prevents user from selecting a player if one is already selected
         if(isPlayerSelected){
             return;
         }
 
+        var chosenCharacter = charClick.currentTarget.id; //not sure how this works, but it grabs the parent div when the image or stats elements are clicked
+
+        //Sets current plater based on user click
         switch (chosenCharacter){
             case "div-char-select-boshek":
                 boshek.player = true;
@@ -129,6 +138,7 @@ function playerSelect() {
         $(".player").append("<h2>" + player.name + "</h2>"); //Add character name to play area
         createCharacterDiv(player, "player"); //Creates player image/stats element
 
+        //Creating opponent list from all other characters not picked
         $(".opp-list").append("<h1>Your Opponents</h1>");
         for(var i in characterArray)
         {
@@ -139,7 +149,8 @@ function playerSelect() {
         }
 
         isPlayerSelected = true;
-        $(".char-select").empty();
+
+        $(".char-select").empty(); //empties character select div to make room for battle area
 
         opponentSelect();
     });
@@ -147,16 +158,19 @@ function playerSelect() {
 
 function opponentSelect() {
 
-    checkForEndGame();
-    $(".game-info").html("<h1>Choose Your Opponent</h1>");
+    checkForEndGame(); //prevents opponent select from occurring after last opp. defeated
+
+    $(".game-info").html("<h1>Choose Your Opponent</h1>"); //shows message while waiting for opp. to be picked
+
 
     $(".img-div-opp-list").click(function (oppClick) {
 
+        //prevents user from selecting opponent if one is already picked
         if (isOppSelected) {
             return;
         }
 
-        var chosenOpponent = oppClick.currentTarget.id;
+        var chosenOpponent = oppClick.currentTarget.id; //sets opponent to character user clicked
 
         switch (chosenOpponent) {
             case "div-opp-list-boshek":
@@ -172,11 +186,11 @@ function opponentSelect() {
                 currentOpponent = lobot;
         }
 
-        $(".middle").append("<h1>Vs.</h1>"); //Add Vs. Text
+        $(".middle").append("<h1>Vs.</h1>"); //Add Vs. Text once opponent is chosen
         $(".current-opponent").append("<h2>" + currentOpponent.name + "</h2>"); //Add Opponent Name to Screen
-        createCharacterDiv(currentOpponent,"current-opponent");
-        isOppSelected = true;
-        $("#div-opp-list-" + currentOpponent.id).hide();
+        createCharacterDiv(currentOpponent,"current-opponent"); //adds opp. image & stats to battle area
+        isOppSelected = true; //sets game state
+        $("#div-opp-list-" + currentOpponent.id).hide(); //removes selected opponent from list of choices
 
         battle();
 
@@ -188,8 +202,8 @@ function opponentSelect() {
 //Is called after both player & opponent have been selected
 function battle() {
 
-    $(".game-info").html("<h1>Fight!</h1>");
-    $(".middle").append("<button class='btn btn-danger btn-lg' id='attack'>Attack</button>");
+    $(".game-info").html("<h1>Fight!</h1>"); //replaces Choose Opponent text
+    $(".middle").append("<button class='btn btn-danger btn-lg' id='attack'>Attack</button>"); //adds attack button to screen
 
     $("#attack").click(function () {
         attack();
@@ -199,48 +213,41 @@ function battle() {
 }
 
 
-//This function creates all of the character divs and places it in the chosen panel
+//This function creates all of the character divs and places it in the chosen panel. This is where the magic happens
 function createCharacterDiv(object, panel){
 
     var div = $("<div class='img img-div-" + panel + "' id='div-" + panel + "-" + object.id + "'>");
     var image = $("<img src='" + object.img + "' class='image image-" + panel + "' id='img-" + panel + "-" + object.id + "'>");
     var stats = $("<p class = 'stats panel-" + panel + "'> Atk: " + object.attack + " HP: " + object.hp + "</p>");
 
-    $("." + panel).append(div);
-    div.append(image, stats);
+    $("." + panel).append(div); //creates initial div
+    div.append(image, stats); //adds character image and stats to div
 
 }
-
-//Ths function finds the parent div and returns its name
-function getParent(event){
-    var element = document.getElementById(event.target.id);
-    var parent = element.parentNode;
-    console.log(parent.id);
-    return parent.id;
-}
-
 
 //Is called when the Attack button is pressed
 function attack() {
 
+    //prevents attacking if opponent is not selected. may not be needed now that the attack button is hidden if opp isn't selected
     if(isOppSelected == false){
         return;
     }
 
-    currentOpponent.hp -= (player.attack * attackCounter);
-    player.hp -= currentOpponent.attack;
-    attackCounter++;
-    $('.panel-player').html("Atk: " + (player.attack * attackCounter) + " HP: " + player.hp);
-    $('.panel-current-opponent').html("Atk: " + currentOpponent.attack + " HP: " + currentOpponent.hp);
+    currentOpponent.hp -= (player.attack * attackCounter); //player attacking opp
+    player.hp -= currentOpponent.attack; //opp attacking player
+    attackCounter++; //increase attack level
+    $('.panel-player').html("Atk: " + (player.attack * attackCounter) + " HP: " + player.hp); //updates player stats
+    $('.panel-current-opponent').html("Atk: " + currentOpponent.attack + " HP: " + currentOpponent.hp); //updates opp stats
 }
 
 
 function checkForOppDefeat(){
-    if(currentOpponent.hp < 1){
-        $(".current-opponent").empty();
-        $(".middle").empty();
-        isOppSelected = false;
+    if(currentOpponent.hp < 1){ //if opp health is 0 or less
+        $(".current-opponent").empty(); //empty current opponent div
+        $(".middle").empty(); //gets rid of attack button and VS. text
+        isOppSelected = false; //sets game state to waiting for opponent
 
+        //updates actual character object to defeated
         for(var i in characterArray){
             if (currentOpponent.id === characterArray[i].id){
                 characterArray[i].defeated = true;
@@ -275,10 +282,10 @@ function checkForEndGame() {
 }
 
 function endGame() {
-    $(".game-info").empty();
-    $(".char-select").html("<h1>Game Over</h1>");
-    $(".middle").append("<button class='btn btn-primary btn-lg' id='restart'>Restart Game</button>");
-    isOppSelected = true;
+    $(".game-info").empty(); //clears Fight! text
+    $(".char-select").html("<h1>Game Over</h1>"); //adds Game Over text
+    $(".middle").append("<button class='btn btn-primary btn-lg' id='restart'>Restart Game</button>"); //creates restart button
+    isOppSelected = true; //prevents user from select opp if defeated
     $("#restart").click(function () {
         gameStart();
     })
